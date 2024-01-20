d8e6347b9e07747e988d8f55db31f2cf65cec54b
Mehran Piltan <110996599+MagicalAtom@users.noreply.github.com>
2024-01-20 14:27:51 +0330

    Create 3-exports.js

---
+++ 3-exports.js
@@ -0,0 +1,23 @@
+// اکسپورت ماژول ها در نود جی اس
+// باید یک نام واسش در نظر بگیریم
+
+
+function get(){
+  
+}
+
+
+
+module.exports = {
+  getfunction:get,
+}
+
+یا 
+
+module.exports.getfunction = get
+
+
+
+تو صفحات دیگه 
+require
+میکنیم روی یک متغیر و استفاده میکنیم 
